822d2bda9ed3fca1f67a75ef364349ebe23e8ec9
Jose De La Cruz <jose.delcruz0490@icloud.com>
2021-08-14 00:49:27 -0400

    Added the files for file module

---
+++ modules.js
@@ -0,0 +1,4 @@
+var _a = require("./people.js"), people = _a.people, ages = _a.ages;
+console.log(people, ages);
+var os = require("os");
+console.log(os.platform(), os.homedirectory());
---
+++ people.js
@@ -0,0 +1,7 @@
+var people = ["mario", "luigi", "yoshi", "bowser", "peach"];
+var ages = [20, 25, 30, 35];
+console.log(people);
+module.exports = {
+    people: people,
+    ages: ages
+};
